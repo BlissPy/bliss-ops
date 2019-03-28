3490e78f4127126ef67857f10c4610059b938e6d
Liam H <35095843+ir-3@users.noreply.github.com>
2019-03-28 13:42:35 -0500

    Header

--- blissops/numpyops.py
+++ blissops/numpyops.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+blissops.numpyops
+=================
+
+Somewhat async image manipulation library 
+created for use within the bliss Discord bot. 
+Makes use of numpy and wand and takes BytesIO 
+as an input and as an output.
+
+:copyright: (c) 2019 Liam (ir-3) H.
+:license: MIT, see LICENSE for more details.
+"""
+
 from io import BytesIO
 
 import skimage
