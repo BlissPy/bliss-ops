90dcebcb777dedcd5c1cab58a1f4a2c9b37a6c31
Liam H <35095843+ir-3@users.noreply.github.com>
2019-03-28 13:42:05 -0500

    Header

--- blissops/imageops.py
+++ blissops/imageops.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+blissops.imageops
+=================
+
+Somewhat async image manipulation library 
+created for use within the bliss Discord bot. 
+Makes use of numpy and wand and takes BytesIO 
+as an input and as an output.
+
+:copyright: (c) 2019 Liam (ir-3) H.
+:license: MIT, see LICENSE for more details.
+"""
+
 import asyncio
 import functools
 from io import BytesIO
