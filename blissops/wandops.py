928327bf53dca6b68980a542feae829555ce09d7
Liam H <35095843+ir-3@users.noreply.github.com>
2019-03-28 13:43:34 -0500

    Header

--- blissops/wandops.py
+++ blissops/wandops.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+blissops.wandops
+================
+
+Somewhat async image manipulation library 
+created for use within the bliss Discord bot. 
+Makes use of numpy and wand and takes BytesIO 
+as an input and as an output.
+
+:copyright: (c) 2019 Liam (ir-3) H.
+:license: MIT, see LICENSE for more details.
+"""
+
 import math
 import platform
 import os
