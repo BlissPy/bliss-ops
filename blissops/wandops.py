9fa133e15b4b9f60cc8f7606359c2193e012a741
Liam H <admin@ir3.me>
2019-03-28 22:04:42 -0500

    Remove unused imports

--- blissops/wandops.py
+++ blissops/wandops.py
@@ -4,17 +4,15 @@
 blissops.wandops
 ================
 
-Somewhat async image manipulation library 
-created for use within the bliss Discord bot. 
-Makes use of numpy and wand and takes BytesIO 
+Somewhat async image manipulation library
+created for use within the bliss Discord bot.
+Makes use of numpy and wand and takes BytesIO
 as an input and as an output.
 
 :copyright: (c) 2019 Liam (ir-3) H.
 :license: MIT, see LICENSE for more details.
 """
 
-import math
-import platform
 import os
 from io import BytesIO
 
